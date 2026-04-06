9df7834047e4f3d4a6d5c292bdc724db5736808f
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2026-04-06 23:23:26 +0200

    Mise à jour du préfixe de route pour WeatherForecast
    Le contrôleur WeatherForecast utilise désormais le préfixe "api/"
    dans sa route, passant de [Route("[controller]")] à
    [Route("api/[controller]")], afin de standardiser les endpoints
    de l’API.

--- ImmoPredict.Api/Controllers/WeatherForecastController.cs
+++ ImmoPredict.Api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ImmoPredict.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries =
